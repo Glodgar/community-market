03ef7121dfd4b9b2091a0156937a8bdb073e4232
Glodgar <glodgarek@gmail.com>
2020-11-27 04:22:19 +0100

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+
+		<Nav />
+		
+		<router-view/>
+	</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+	import Nav from '../src/components/Nav';
 
-#nav {
-  padding: 30px;
+	export default {
+		name: 'app',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+		components: {
+			Nav,
+		}
+	}
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang="scss" scooped>
+    .app {
+        font-family: 'Roboto', sans-serif;
+        margin: 0;
+        padding: 0;
+        color: #ffffff;
     }
-  }
-}
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="navigationBar">
+        <div class="fixedBar">
+            <div class="nav">
+
+                <router-link to="/">Home</router-link>
+                <router-link to="/about">About</router-link>
+                <router-link to="/login">Login</router-link>
+
+            </div>
+        </div>
+        <div class="staticBar"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav'
+}
+</script>
+
+<style lang="scss" scooped>
+    .fixedBar {
+        height: 50px;
+        width: 100%;
+        background-color: rgba(250, 250, 250, 0.5);
+        position: fixed;
+        z-index: 10;
+
+        font-weight: bold;
+        line-height: 50px;
+
+        border-bottom: 1px solid rgba(250, 150, 0, 1);
+    }
+
+    .nav {
+        height: 50px;
+        display: flex;
+        right: 0;
+
+        position: fixed;
+    }
+
+    .nav a {
+        display: block;
+        color: #000000;
+        padding: 0 20px;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-weight: bold;
+        line-height: 50px;
+        width: 100px;
+        text-align: center;
+    }
+
+    .nav a:hover {
+        color: rgba(250, 150, 0, 1);
+    }
+
+    .nav a.router-link-exact-active {
+        text-decoration: underline 2px rgba(250, 150, 0, 1);
+    }
+    
+    .staticBar {
+        width: 100%;
+        height: 50px;
+        background-color: #ffffff;
+    }
+</style>
